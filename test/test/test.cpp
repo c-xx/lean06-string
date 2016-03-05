1ff01414f1fe89f0234447528824c562f151e881
edwin <sblig@126.om>
2016-03-05 19:34:50 +0800

    string 操作

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <stdlib.h>
+#include <string>
 using namespace std;
 
 
 int main(void) {
 
+	string name;
+	cout << "please input your name:";
+	getline(cin, name);
+	if (name.empty())
+	{
+		cout << "input is null .." << endl;
+		system("pause");
+		return 0;
+	}
+
+	if (name == "imooc")
+	{
+		cout << "you are a administrator" << endl;
+	}
+
+	cout << "hello " + name << endl;
+	cout << "you name length:" << name.size() << endl;
+	cout << "you name firest letter:" << name[0] << endl;
 
 	system("pause");
 
